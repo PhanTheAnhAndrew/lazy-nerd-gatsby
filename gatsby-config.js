d9e9bc2c028f9ead2b36a04b830dfd9192df7142
AndrewPhanUIT <andrew.phan.uit@gmail.com>
2022-04-05 01:15:42 +0700

    Remove sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,6 @@ module.exports = {
     plugins: [
         "gatsby-plugin-styled-components",
         "gatsby-plugin-react-helmet",
-        "gatsby-plugin-sitemap",
         "gatsby-plugin-image",
         "gatsby-plugin-offline",
         "gatsby-plugin-sharp",
